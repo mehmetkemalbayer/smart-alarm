0d5c9fb3af1d26a3370ce33fdcd3b4619ba076b5
Mehmet Kemal Bayer <kemal.bayer@gmail.com>
2017-08-15 23:58:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const FITBIT_CLIENT_ID = process.env.FITBIT_CLIENT_ID;
+const FITBIT_CLIENT_SECRET = process.env.FITBIT_CLIENT_SECRET;
+const CALLBACK_URL = process.env.callbackURL;
+
+var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
+ 
+passport.use(new FitbitStrategy({
+    clientID:     FITBIT_CLIENT_ID,
+    clientSecret: FITBIT_CLIENT_SECRET,
+    callbackURL: CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+app.get('/auth/fitbit',
+  passport.authenticate('fitbit', { scope: ['activity','heartrate','location','profile'] }
+));
+ 
+app.get( '/auth/fitbit/callback', passport.authenticate( 'fitbit', { 
+        successRedirect: '/auth/fitbit/success',
+        failureRedirect: '/auth/fitbit/failure'
+}));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
